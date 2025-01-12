6a92f1f38593530829786324d684eede900f9ae2
Robert Hecker <robert.hecker@stud.h-da.de>
2025-01-13 00:03:43 +0100

    Move audio receiving to external worker thread to decouple it from the UI

--- main.py
+++ main.py
@@ -1,10 +1,57 @@
 import sys
-from typing import Mapping, Any
+from typing import Mapping, Any, Optional
 
 import numpy as np
 import pyaudio
-from PySide6.QtCore import QSize
+from PySide6.QtCore import QThread, Signal
 from PySide6.QtWidgets import QApplication, QMainWindow, QComboBox, QVBoxLayout, QLabel, QWidget
+
+
+class PyAudioWorker(QThread):
+    audio_chunk_received = Signal(float)
+
+    def __init__(self, audio: pyaudio.PyAudio, device: Mapping[str, Any], /, parent=None):
+        super().__init__(parent=parent)
+        self.pyaudio = audio
+        self.device = device
+
+        self.running = False
+        self.stream: Optional[pyaudio.Stream] = None
+
+    def _on_audio_received(self, in_data: bytes | None, frame_count: int, time_info: Mapping[str, float], status: int):
+        num_bytes = len(in_data)
+        if num_bytes <= 0:
+            return
+
+        data: np.ndarray = np.frombuffer(in_data, np.int16)
+        new_data = (data / float(np.iinfo(np.int16).max)).clip(min=-1.0, max=1.0)
+        # print(f'{new_data.min()=}, {new_data.max()=}')
+        max_value = new_data.max()
+        print(max_value)
+
+        self.audio_chunk_received.emit(max_value)
+
+        return None, (pyaudio.paContinue if self.running else pyaudio.paComplete)
+
+    def stop(self):
+        if self.stream is not None:
+            self.stream.stop_stream()
+        self.running = False
+
+    def run(self):
+        self.running = True
+
+        index = self.device['index']
+        sample_rate = int(self.device['defaultSampleRate'])
+
+        self.stream = self.pyaudio.open(
+            rate=sample_rate,
+            channels=1,
+            format=pyaudio.paInt16,
+            input=True,
+            input_device_index=index,
+            stream_callback=self._on_audio_received
+        )
 
 
 class MainWindow(QMainWindow):
@@ -32,22 +79,18 @@ class MainWindow(QMainWindow):
         container.setLayout(layout)
         self.setCentralWidget(container)
 
-        self.stream = self._create_stream(default_device)
-        self.stream.start_stream()
+        self.worker: Optional[PyAudioWorker] = self._create_and_start_worker(self.pyaudio, default_device)
 
-    def _create_stream(self, device: Mapping[str, Any]):
-        print(device)
-        index = device['index']
-        sample_rate = int(device['defaultSampleRate'])
+    def _create_and_start_worker(self, audio: pyaudio.PyAudio, device: Mapping[str, Any]):
+        worker = PyAudioWorker(audio, device, parent=self)
+        worker.audio_chunk_received.connect(self._update_volume_label)
+        # TODO: handle exceptions when this fails, e. g. when device is not available or similar and change back to previous or any other device in UI!
+        worker.start()
 
-        return self.pyaudio.open(
-            rate=sample_rate,
-            channels=1,
-            format=pyaudio.paInt16,
-            input=True,
-            input_device_index=index,
-            stream_callback=self._on_pyaudio_received
-        )
+        return worker
+
+    def _update_volume_label(self, max_value: float):
+        self.volume_label.setText(str(max_value))
 
     def _on_device_changed(self, index: int):
         device_name = self.audio_box.itemText(index)
@@ -55,26 +98,10 @@ class MainWindow(QMainWindow):
         device = self.pyaudio.get_device_info_by_index(index)
         print(device_name, device_index, device['maxInputChannels'])
 
-        if self.stream is not None:
-            self.stream.close()
+        if self.worker is not None:
+            self.worker.stop()
 
-        # TODO: handle exceptions when this fails, e. g. when device is not available or similar and change back to previous or any other device in UI!
-        self.stream = self._create_stream(self.pyaudio.get_device_info_by_index(device_index))
-        self.stream.start_stream()
-
-    def _on_pyaudio_received(self, in_data: bytes | None, frame_count: int, time_info: Mapping[str, float], status: int):
-        num_bytes = len(in_data)
-        bytes_per_sample = 2
-        bytes_per_frame = 2
-        num_samples = num_bytes // bytes_per_sample
-
-        data: np.ndarray = np.frombuffer(in_data, np.int16)
-        new_data = (data / float(np.iinfo(np.int16).max)).clip(min=-1.0, max=1.0)
-        #print(f'{new_data.min()=}, {new_data.max()=}')
-        max_value = new_data.max()
-        print(max_value)
-        self.volume_label.setText(str(max_value))
-        return None, pyaudio.paContinue
+        self.worker = self._create_and_start_worker(self.pyaudio, device)
 
     def _get_valid_input_devices(self):
         valid_input_devices = []
