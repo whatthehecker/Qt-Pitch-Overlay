0eb0c0dd897dd6d5f3c344bd3e7367dcb1c25561
Robert Hecker <robert.hecker@stud.h-da.de>
2025-02-03 19:32:01 +0100

    Dynamically resize text for frequency display

--- audio_display_widget.py
+++ audio_display_widget.py
@@ -1,8 +1,8 @@
 from collections import deque
 
 from PySide6.QtCharts import QChartView, QChart, QLineSeries, QValueAxis, QCategoryAxis
-from PySide6.QtCore import Qt, QPointF
-from PySide6.QtGui import QBrush, QColor
+from PySide6.QtCore import Qt, QPointF, QPoint, QRect, QMargins
+from PySide6.QtGui import QBrush, QColor, QFontMetrics, QFont
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QSizePolicy
 
 
@@ -38,13 +38,19 @@ class AudioDisplayWidget(QWidget):
         self.setSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
 
         self.volume_label = QLabel('<volume here>', self)
-        self.volume_label.setStyleSheet('QLabel { background-color : red; color : blue; }')
+        #  "border: 1px solid black;" is good for debugging bounds
+        self.volume_label.setStyleSheet('QLabel { color: #111; }')
+        self.volume_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.volume_label.setContentsMargins(0, 0, 0, 0)
+        #self.volume_label.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.volume_label.setScaledContents(True)
         #self.volume_label.setFixedSize(100, 20)
 
     def _create_y_axis(self, range_min: int, range_max: int, target_min: int, target_max: int) -> QCategoryAxis:
         y_axis = QCategoryAxis()
         y_axis.setRange(range_min, range_max)
         y_axis.setLabelsPosition(QCategoryAxis.AxisLabelsPosition.AxisLabelsPositionOnValue)
+        y_axis.setLabelsVisible(False)
         # Mark the pink target region by creating an axis with "checkered" regions and only show three levels,
         # effectively creating only a single region with a pink backdrop.
         y_axis.setShadesBrush((QBrush(QColor(245, 169, 184, 0x55))))
@@ -64,7 +70,21 @@ class AudioDisplayWidget(QWidget):
     def resizeEvent(self, event):
         super().resizeEvent(event)
         # Move frequency label on top of chart:
-        self.volume_label.move(self._chart_view.geometry().center())
+        MARGIN = 40
+        new_geometry = self._chart_view.geometry().marginsRemoved(QMargins(MARGIN, MARGIN, MARGIN, MARGIN))
+        self.volume_label.setGeometry(new_geometry)
+
+        # Determine optimal font size by increasing font size until it does not fit the new bounds anymore.
+        font = QFont()
+        font_size = 1
+        while True:
+            font.setPixelSize(font_size)
+            rect = QFontMetrics(font).boundingRect('999Hz')
+            if rect.height() > new_geometry.height() or rect.width() > new_geometry.width():
+                break
+            font_size += 1
+        font.setPixelSize(font_size)
+        self.volume_label.setFont(font)
 
     def add_value(self, x: float, y: float):
         self._buffer.append(QPointF(x, y))
@@ -76,7 +96,7 @@ class AudioDisplayWidget(QWidget):
         self._chart.axisX().setRange(x - 10, x + 1)
 
         if y > 0:
-            self.volume_label.setText(f'{y:0.4f}')
+            self.volume_label.setText(f'{round(y)}Hz')
         # x = list(self.buffer)
         # y = [float(x) for x in range(len(self.buffer))]
         # self._series.appendNp(x, y)
